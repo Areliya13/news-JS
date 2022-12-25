dc4f964f542eb1bec9b4ab26e3cd55afe387f182
Areliya13 <sharipovatamara@gmail.com>
2022-12-25 17:50:25 +0300

    fix: API key fixed

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,13 +1,13 @@
 import Loader from './loader';
 
-const key1: number = 0x2485f009a4ab;
+const key: string = '2485f009a4ab4482936adb0991cdbf26';
 const key2: number = 0x4482936adb09;
 const key3: number = 0x91cdbf26;
 
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: key1.toString(16).concat(key2.toString(16), key3.toString(16)),
+            apiKey: key,
         });
     }
 }
